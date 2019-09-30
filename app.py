d8ff51cbd2af9b44cbac7e40d39ede09a2ec85f6
David Smiles <davidugbero@gmail.com>
2019-09-30 16:51:29 +0100

    implement get_store and get_stores endpoint

--- app.py
+++ app.py
@@ -1,12 +1,66 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# POST - used to receive data and we have to deal with it
+# GET - used to send data back only
 
-# endpoint => the request
-@app.route('/')     # http://www.google.com/    -> homepage endpoint
-def home():
-    return 'hello david james' # the response
+# from the browser's perspective, this is the opposite
+# the browser will use POST to send us data
+# and use GET to receive data
+
+stores = [
+    {
+        'name': 'teclado',
+        'items': [
+            {
+                'name': 'REST API with Python and Flask',
+                'price': 11.99
+            }
+        ]
+    },
+    {
+        'name': 'shitty',
+        'items': [
+            {
+                'name': 'REST API with Python and Flask',
+                'price': 11.99
+            }
+        ]
+    }
+]
+
+
+# POST /store {name}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    data = [store for store in stores if store['name'] == name]
+    return jsonify(data)
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# POST /store/<string:name>/item {name, price}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 
 if __name__ == '__main__':
