b40f7c41113154617f999bd61eb42f4806444664
David Smiles <davidugbero@gmail.com>
2019-10-01 18:06:58 +0100

    add controller code for user auth

---
+++ restful/security.py
@@ -0,0 +1,21 @@
+from werkzeug.security import safe_str_cmp
+from restful.user import User
+
+
+users = [
+    User(1, 'david', 'smiles')
+]
+
+username_mapping = {user.username: user for user in users}
+userid_mapping = {user.id: user for user in users}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    userid = payload['identity']
+    return userid_mapping.get(userid, None)
